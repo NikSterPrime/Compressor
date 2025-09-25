7048edb704b2712faf6a402ce9eb8dd333e6db29
Aditya Puranik <120474074+NikSterPrime@users.noreply.github.com>
2025-09-25 18:20:43 +0530

    CLI command added

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        cout<<"Usage: \n";
+        cout<<" compress <input_file> <output_file>\n";
+        cout<<" decompress <input_file> <output_file>\n";
+        return 1;
+    }
+
+    string command = argv[1];
+
+    if(command == "compress")
+    {
+        if(argc < 4)
+        {
+            cout<<"Missing input/output files";
+            return 1;
+        }
+        string inputFile = argv[2];
+        string outputFile = argv[3];
+
+
+        cout<<"Compressing "<<inputFile<<" to "<<outputFile<<"...Coming soon!\n";
+    }
+
+    else if(command == "decompress")
+    {
+        if(argc < 4)
+        {
+            cout<<"Missing input/output files";
+            return 1;
+        }
+        string inputFile = argv[2];
+        string outputFile = argv[3];
+
+
+        cout<<"Decompressing "<<inputFile<<" to "<<outputFile<<"...Coming soon!\n";
+    }
+
+    else
+    {
+        cout<<"Unknown command: "<<command<<"\n";
+        cout<<"Use compress or decompress .\n";
+    }
+
+    return 0;
+}

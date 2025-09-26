d94de8c422d213081ed45461cf8badb1aa2faf00
Aditya Puranik <120474074+NikSterPrime@users.noreply.github.com>
2025-09-26 18:23:23 +0530

    Update huffman.cpp

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <huffman.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+class Node {
+	public:
+		int data;
+		Node *left,*right;
+		Node(int x)
+		{
+			data = x;
+			left = nullptr;
+			right = nullptr;
+		}
+};
 
 void compressHuffman(const string& inputFile,const string& outputFile)
 {
-    cout << "[Huffman] Ccompressing " << inputFile << " -> " << outputFile << " (Coming soon!)\n";
+		
 }
 
-void decompressHuffman(const string& inputFile,const string& outputFile)
-{
-    cout << "[Huffman] Decompressing " << inputFile << " -> " << outputFile << " (Coming soon!)\n";
-}

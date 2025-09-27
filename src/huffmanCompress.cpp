36aeb9cfd4273c4e786bc4257369177ff75a3469
Aditya Puranik <120474074+NikSterPrime@users.noreply.github.com>
2025-09-27 13:18:02 +0530

    File input taken instead of string

--- src/huffmanCompress.cpp
+++ src/huffmanCompress.cpp
@@ -1,14 +1,26 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include "../include/huffmanCompress.h"
 #include <unordered_map>
+#include "../include/huffmanCompress.h"
 #include "../include/utils.h"
 using namespace std;
 
-void compress(const string& inputFile,const string& outputFile)
+void compress(const string& inFile,const string& outFile)
 {
-
-		auto frequencyTable = calculateFrequencies(inputFile);
+		ifstream inputFile(inFile);
+		string line_content;
+		string file_contents;
+		if(!inputFile.is_open())
+		{
+			cerr<<"Error Opening the file !"<<endl;
+			exit(0);
+		}
+		while(getline(inputFile,line_content))
+		{
+			file_contents+=line_content;
+		}
+		auto frequencyTable = calculateFrequencies(file_contents);
 		for(const auto& i:frequencyTable)
 		{
 			cout<<i.first<<" "<<i.second;
